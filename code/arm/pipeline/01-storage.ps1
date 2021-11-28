adee491f584bad1c14c068ab364165636e952e35
test <test@test.com>
2021-11-28 17:51:46 +0530

    arm checkin 04

---
+++ code/arm/pipeline/01-storage.ps1
@@ -0,0 +1,14 @@
+$rg = "arm-introduction-01"
+Get-AzResourceGroup -Name $rg -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if ($notPresent)
+{
+    # ResourceGroup doesn't exist
+    New-AzResourceGroup -Name $rg -Location northeurope -Force 
+}
+else
+{
+    # ResourceGroup exist
+}
+
+New-AzResourceGroupDeployment -Name "new-Storage" -ResourceGroupName $rg -TemplateFile ".\01-storage.json"
+    
